bb828d4b44ff7f76bee2c492db9b2970f512bade
prateek sharma <ppprateeksharma006@gmail.com>
2024-07-04 21:31:18 +0530

    created a demo ui for product detail page

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -1,3 +1,4 @@
+import 'package:ecom/widget/support_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetail extends StatefulWidget {
@@ -13,26 +14,109 @@ class _ProductDetailState extends State<ProductDetail> {
     return Scaffold(
       backgroundColor: Color(0xFFfef5f1),
       body: Container(
-        padding: EdgeInsets.only(top: 50, left: 20),
+        padding: EdgeInsets.only(
+          top: 50,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(),
-                    borderRadius: BorderRadius.circular(30)),
-                child: Icon(Icons.arrow_back_ios_new_outlined),
-              ),
+            Stack(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 20),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Icon(Icons.arrow_back_ios_new_outlined),
+                  ),
+                ),
+                Center(
+                  child: Image.asset(
+                    "images/headphone2.png",
+                    height: 400,
+                  ),
+                ),
+              ],
             ),
-            Center(
-              child: Image.asset(
-                "images/headphone2.png",
-                height: 300,
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Headphone",
+                          style: AppStyle.boldTextFieldStyle(),
+                        ),
+                        Text(
+                          "\$300",
+                          style: TextStyle(
+                            color: Color(0xFFfd6f3e),
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Details",
+                      style: AppStyle.semiBoldTextFieldStyle(),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Elevate your auditory experience with the SonicWave Pro 500 Wireless Headphones, where cutting-edge technology meets unparalleled comfort. Whether you’re a music aficionado, a dedicated gamer, or a professional seeking superior audio clarity for calls, the SonicWave Pro 500 is engineered to exceed your expectations.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 90,
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFfd6f3e),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(
+                        child: Text(
+                          "Buy Now",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
               ),
             ),
           ],
